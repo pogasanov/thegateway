1772762a2d58e8d6d36ac08cd2e7d5bda8cf238d
pogasanov <pogasanov@gmail.com>
2020-03-27 15:04:57 +0700

    can fetch prestashop products

--- prestashop/PrestashopImporter.py
+++ prestashop/PrestashopImporter.py
@@ -1,2 +1,14 @@
+import os
+
+import requests
+
+
 class PrestashopImporter:
-    pass
+    def __init__(self):
+        self.API_HOSTNAME = os.environ.get('PRESTASHOP_HOSTNAME', 'http://127.0.0.1:8080')
+        self.API_KEY = os.environ.get('PRESTASHOP_API_KEY', 'RZY9E7L8AP5EPSMDZSXQ2SDJXZCEXBU4')
+
+    def fetch_products(self):
+        result = requests.get(f'{self.API_HOSTNAME}/api/products', auth=(self.API_KEY, ''),
+                              params={'output_format': 'JSON'})
+        return result.json()['products']
--- prestashop/tests/PrestashopImporterTest.py
+++ prestashop/tests/PrestashopImporterTest.py
@@ -1,8 +1,24 @@
 from unittest import TestCase
 
+import responses
+
 from prestashop.PrestashopImporter import PrestashopImporter
 
 
 class PrestashopImporterTest(TestCase):
-    def test_can_init(self):
-        PrestashopImporter()
+    @classmethod
+    def setUpClass(cls):
+        cls.importer = PrestashopImporter()
+
+    @responses.activate
+    def test_can_fetch_prestashop_products(self):
+        PRODUCTS = [{'id': '1'}, {'id': '2'}]
+
+        responses.add(responses.GET,
+                      'http://127.0.0.1:8080/api/products?output_format=JSON',
+                      json={'products': PRODUCTS},
+                      status=200)
+
+        products = self.importer.fetch_products()
+        self.assertEqual(len(products), len(PRODUCTS))
+        self.assertEqual(products, PRODUCTS)
