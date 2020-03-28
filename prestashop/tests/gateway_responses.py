ee801e1050433f328c21423dd4982592ed1e3fa8
pogasanov <pogasanov@gmail.com>
2020-03-28 21:52:39 +0700

    move api responses to separate modules

---
+++ prestashop/tests/gateway_responses.py
@@ -0,0 +1,48 @@
+GATEWAY_PRODUCT = {
+    "activated": None,
+    "brief": None,
+    "created": "2020-03-28T08:15:07.587983+00:00",
+    "data": {
+        "imageFiles": [],
+        "videos": []
+    },
+    "desc": None,
+    "guid": "9fea73c7-2be4-4f6c-903c-10cbac9dbeb5",
+    "images": [],
+    "name": "test23",
+    "owner_guid": "a547de18-7a1d-450b-a57b-bbf7f177db84",
+    "price": {
+        "currency": "zł",
+        "final": {
+            "unit": {
+                "amount": 1.00,
+                "currency": "zł",
+                "vat": 0.19,
+                "vat0": 0.81,
+                "vat_percent": 23
+            }
+        },
+        "original": {
+            "campaign_modifier": None,
+            "unit": {
+                "amount": 1.00,
+                "currency": "zł",
+                "vat": 0.19,
+                "vat0": 0.81,
+                "vat_percent": 23
+            }
+        },
+        "vat_class": 23
+    },
+    "pricing": None,
+    "size": None,
+    "sku": None,
+    "tag_guids": [],
+    "tags": [],
+    "timestamps": {
+        "created": "2020-03-28T08:15:08.730176+00:00"
+    },
+    "type": "default",
+    "vat": "VAT23",
+    "weight": None
+}
