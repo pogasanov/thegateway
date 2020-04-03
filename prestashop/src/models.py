de52f7375aee4f411d9c2eef92e68c220df673a2
pogasanov <pogasanov@gmail.com>
2020-04-03 20:28:32 +0700

    proper model for images

--- prestashop/src/models.py
+++ prestashop/src/models.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, BinaryIO
+
+
+@dataclass
+class Image:
+    filename: str
+    mimetype: str
+    data: BinaryIO
 
 
 @dataclass
@@ -10,5 +17,5 @@ class Product:
     description: str = ''
     description_short: str = ''
     sku: str = ''
-    images: List[object] = field(default_factory=list)
+    images: List[Image] = field(default_factory=list)
     variant_data: dict = field(default_factory=dict)
