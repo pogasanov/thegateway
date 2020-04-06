20fb47f726614a4c799b55cfda8241018d358599
Gerard Wiśniewski <gerard.wisniewski@profil-software.com>
2020-04-06 18:07:38 +0200

    Run black on all files

--- prestashop/src/models.py
+++ prestashop/src/models.py
@@ -16,8 +16,8 @@ class Product:
     name: str
     price: float
     stock: float = 0
-    description: str = ''
-    description_short: str = ''
-    sku: str = ''
+    description: str = ""
+    description_short: str = ""
+    sku: str = ""
     images: List[Image] = field(default_factory=list)
     variant_data: dict = field(default_factory=dict)
--- prestashop/tests/gateway_responses.py
+++ prestashop/tests/gateway_responses.py
@@ -2,10 +2,7 @@ GATEWAY_PRODUCT = {
     "activated": None,
     "brief": None,
     "created": "2020-03-28T08:15:07.587983+00:00",
-    "data": {
-        "imageFiles": [],
-        "videos": []
-    },
+    "data": {"imageFiles": [], "videos": []},
     "desc": None,
     "guid": "9fea73c7-2be4-4f6c-903c-10cbac9dbeb5",
     "images": [],
@@ -13,38 +10,22 @@ GATEWAY_PRODUCT = {
     "owner_guid": "a547de18-7a1d-450b-a57b-bbf7f177db84",
     "price": {
         "currency": "zł",
-        "final": {
-            "unit": {
-                "amount": 1.00,
-                "currency": "zł",
-                "vat": 0.19,
-                "vat0": 0.81,
-                "vat_percent": 23
-            }
-        },
+        "final": {"unit": {"amount": 1.00, "currency": "zł", "vat": 0.19, "vat0": 0.81, "vat_percent": 23}},
         "original": {
             "campaign_modifier": None,
-            "unit": {
-                "amount": 1.00,
-                "currency": "zł",
-                "vat": 0.19,
-                "vat0": 0.81,
-                "vat_percent": 23
-            }
+            "unit": {"amount": 1.00, "currency": "zł", "vat": 0.19, "vat0": 0.81, "vat_percent": 23},
         },
-        "vat_class": 23
+        "vat_class": 23,
     },
     "pricing": None,
     "size": None,
     "sku": None,
     "tag_guids": [],
     "tags": [],
-    "timestamps": {
-        "created": "2020-03-28T08:15:08.730176+00:00"
-    },
+    "timestamps": {"created": "2020-03-28T08:15:08.730176+00:00"},
     "type": "default",
     "vat": "VAT23",
-    "weight": None
+    "weight": None,
 }
 GATEWAY_TAG = {
     "active": False,
@@ -58,5 +39,5 @@ GATEWAY_TAG = {
     "parent_guid": "7328d05e-cee7-5ad6-bb04-70dece680b84",
     "product_guids": [],
     "subtags": [],
-    "type": "variant"
+    "type": "variant",
 }
