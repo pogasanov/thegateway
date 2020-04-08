a60a4d0a7219140eca0efdc3c40fb18e6a1aebbd
pogasanov <pogasanov@gmail.com>
2020-04-08 11:27:26 +0700

    set default prestashop language id to None

--- prestashop/Runner.py
+++ prestashop/Runner.py
@@ -11,7 +11,7 @@ from prestashop.importers import Prestashop
 def run_import():
     PRESTASHOP_BASE_URL = os.environ.get("PRESTASHOP_BASE_URL", "http://127.0.0.1:8080")
     PRESTASHOP_API_KEY = os.environ.get("PRESTASHOP_API_KEY", "RZY9E7L8AP5EPSMDZSXQ2SDJXZCEXBU4")
-    LANGUAGE_ID = os.environ.get("PRESTASHOP_LANGUAGE_ID")
+    LANGUAGE_ID = os.environ.get("PRESTASHOP_LANGUAGE_ID", None)
 
     GATEWAY_BASE_URL = os.environ.get("GATEWAY_BASE_URL", "https://sma.dev.gwapi.eu")
     GATEWAY_SHOP_ID = os.environ.get("GATEWAY_SHOP_ID", "a547de18-7a1d-450b-a57b-bbf7f177db84")
