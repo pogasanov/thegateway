3bcaf3ac67f6fdbd63312a1c80dbcc66b129ef70
karzio <karol.ziolkowski@profil-software.com>
2020-04-24 17:16:41 +0200

    Function to get categories that do not match any in gateway categories to map only missing categories instead of going through them all

--- shoper_pkg/shoper/runner.py
+++ shoper_pkg/shoper/runner.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from pathlib import Path
+from typing import List, Dict
 
 import click
 from dotenv import load_dotenv
@@ -19,29 +20,54 @@ SHOPER_USERNAME = os.environ.get("SHOPER_USERNAME", "karol.ziolkowski@profil-sof
 SHOPER_PASSWORD = os.environ.get("SHOPER_PASSWORD", "Magik123")
 
 
+def get_missing_categories(gateway_categories: Dict, importer_categories: List) -> List:
+    """
+    Return list of categories that do not have their representation
+    in gateway categories.
+    """
+    categories = list()
+    for category in importer_categories:
+        if not gateway_categories.get(category):
+            categories.append(category)
+    return categories
+
 @click.command()
 def prepare_mapping_file():
+    """
+    Creates file which maps categories from the ecommerce to the gateway
+    categories. Prints out categories from gateway with their ids
+    and then prompts user to connect ecommerce category with gateway category
+    by passing the id.
+    Based on that the file is generated.
+    """
     importer = Shoper(SHOPER_BASE_URL, SHOPER_USERNAME, SHOPER_PASSWORD)
     gateway = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET, IMAGE_URL_PREFIX)
     gateway_categories = gateway._get_categories()
     importer_categories = importer.get_categories_list()
-    reverted_gateway_categories = dict()
-    filename = input("File name:")
-    for name, _id in gateway_categories:
-        print(f"{_id}: {name}")
-        reverted_gateway_categories.update({_id: name})
-    print("Pass id of a category to be mapped: \n")
-    categories = dict()
-    for category in importer_categories:
-        _id = input(f"{category}: ")
-        categories.update({category: reverted_gateway_categories.get(_id)})
-    with open(f"{filename}.txt") as file:
-        file.write(json.dumps(categories))
+    missing_categories = get_missing_categories(gateway_categories, importer_categories)
+    if missing_categories:
+        reverted_gateway_categories = dict()
+        filename = input("File name:")
+        for name, _id in gateway_categories:
+            print(f"{_id}: {name}")
+            reverted_gateway_categories.update({_id: name})
+        print("Pass id of a category to be mapped: \n")
+        categories = dict()
+        for category in missing_categories:
+            _id = input(f"{category}: ")
+            categories.update({category: reverted_gateway_categories.get(_id)})
+        with open(f"{filename}.txt") as file:
+            file.write(json.dumps(categories))
+    else:
+        print("All categories are covered with categories from the gateway.")
 
 
 @click.command()
 @click.option("--path", "-p")
 def sync_categories(path):
+    """
+    Pass path to a file with mapped categories.
+    """
     with open(path) as file:
         importer = Shoper(SHOPER_BASE_URL, SHOPER_USERNAME, SHOPER_PASSWORD)
         gateway = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET, IMAGE_URL_PREFIX)
@@ -53,7 +79,12 @@ def sync_categories(path):
                 importer_product = importer.fetch_product(product)
                 category = importer_categories.get(importer_product.get("category_id"))
                 mapped_category = categories.get(category)
-                product.category = [mapped_category]
+                if mapped_category:
+                    product.category = [mapped_category]
+                # if mapped category is None then the category is covered by gateway categories
+                # or there was no matching category
+                else:
+                    product.category = [category]
                 # TODO gateway.update(product)
 
 
