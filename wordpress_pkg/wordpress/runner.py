8c9abc1ad3635595cf53169735d53fc1fc4c424a
Patryk Kaźmierczak <patryk.kazmierczak@profil-software.com>
2020-04-24 16:17:35 +0200

    Add generating category map

--- wordpress_pkg/wordpress/runner.py
+++ wordpress_pkg/wordpress/runner.py
@@ -1,23 +1,80 @@
+import datetime
+import json
+import logging
 import os
+import pathlib
+import urllib.parse
 
 import click
 from gateway.gateway import Gateway
 from wordpress.importers import WoocommerceWordPress
 
+logger = logging.getLogger(__name__)
+
+WOOCOMMERCE_CONSUMER_KEY = os.environ.get("WOOCOMMERCE_CONSUMER_KEY")
+WOOCOMMERCE_CONSUMER_SECRET = os.environ.get("WOOCOMMERCE_CONSUMER_SECRET")
+WOOCOMMERCE_BASE_URL = os.environ.get("WOOCOMMERCE_BASE_URL")
+
+GATEWAY_BASE_URL = os.environ.get("GATEWAY_BASE_URL")
+GATEWAY_SHOP_ID = os.environ.get("GATEWAY_SHOP_ID")
+GATEWAY_SECRET = os.environ.get("GATEWAY_SECRET")
+IMAGE_URL_PREFIX = f"{os.environ.get('IMAGEBUCKET_URL')}{GATEWAY_SHOP_ID}/"
+
+
+def _map_categories(api_categories, gw_categories) -> dict:
+    mapped_categories = dict()
+    gw_categories_lower = list(map(str.lower, gw_categories))
+    for api_category in map(str.lower, api_categories):
+        if api_category in gw_categories_lower:
+            mapped_categories[api_category] = api_category
+        else:
+            mapped_categories[api_category] = None
+
+    return mapped_categories
+
+
+def _get_category_map(category_map_path: str) -> dict:
+    current_directory = pathlib.Path(__file__).parent.absolute()
+    try:
+        with open(os.path.join(current_directory, category_map_path)) as category_map_file:
+            category_map = json.load(category_map_file)
+            return category_map
+    except FileNotFoundError:
+        print(pathlib.Path(__file__).parent.absolute())
+        logger.error("File with category map '%s' not found.", category_map_path)
+        return None
+
 
 @click.command()
-def run_import():
-    woocommerce_consumer_key = os.environ.get("WOOCOMMERCE_CONSUMER_KEY", "ck_7311e697cff297153143761909b9127188874571")
-    woocommerce_consumer_secret = os.environ.get(
-        "WOOCOMMERCE_CONSUMER_SECRET", "cs_26ffecf428f177897fa87afb5b758fd9cb10c8d2"
-    )
-    woocommerce_base_url = os.environ.get("WOOCOMMERCE_BASE_URL", "http://a41bcc60.ngrok.io")
+@click.option("--prepare-mapping-file", is_flag=True, help="Some help message")
+def prepare_mapping_file():
+    current_directory = pathlib.Path(__file__).parent.absolute()
+    parsed_url = urllib.parse.urlparse(WOOCOMMERCE_BASE_URL)
+    category_mapper_filename = "{domain}_category_map.json".format(domain=parsed_url.netloc)
+    importer = WoocommerceWordPress(WOOCOMMERCE_CONSUMER_KEY, WOOCOMMERCE_CONSUMER_SECRET, WOOCOMMERCE_BASE_URL, None)
+    exporter = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET, IMAGE_URL_PREFIX)
+    api_categories = importer.get_categories()
+    gw_categories = exporter._get_categories()
 
-    gateway_base_url = os.environ.get("GATEWAY_BASE_URL", "https://sma.dev.gwapi.eu")
-    gateway_shop_id = os.environ.get("GATEWAY_SHOP_ID", "20c36f95-3bc0-4d66-bb28-9b16ae1146ab")
-    gateway_secret = os.environ.get("GATEWAY_SECRET", "feWUIqvODUBvzpISShCxtA9BzAEZ4bAn0CMIfTcFgj8=")
-    image_url_prefix = f"{os.environ.get('IMAGEBUCKET_URL')}{gateway_shop_id}/"
-    importer = WoocommerceWordPress(woocommerce_consumer_key, woocommerce_consumer_secret, woocommerce_base_url)
-    exporter = Gateway(gateway_base_url, gateway_shop_id, gateway_secret, image_url_prefix)
+    mapped_categories = _map_categories(api_categories, gw_categories)
+    with open(category_mapper_filename, "w") as output_file:
+        json.dump(mapped_categories, output_file, indent=4)
+
+    print("Generated category map: ", os.path.join(current_directory, category_mapper_filename))
+
+
+@click.command()
+@click.option("--filename", help="Some help text")
+def run_import(filename):
+    print(filename)
+    category_map = _get_category_map(filename)
+    if not category_map:
+        return
+    print(WOOCOMMERCE_CONSUMER_KEY)
+    importer = WoocommerceWordPress(
+        WOOCOMMERCE_CONSUMER_KEY, WOOCOMMERCE_CONSUMER_SECRET, WOOCOMMERCE_BASE_URL, category_map
+    )
+    exporter = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET, IMAGE_URL_PREFIX)
+
     for product_variants in importer.get_products():
         exporter.create_products(product_variants)
