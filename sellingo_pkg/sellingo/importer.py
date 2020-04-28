001d53cd9745f8188c7bf5fe0fe6a8bdb5869af8
Gerard Wiśniewski <gerard.wisniewski@profil-software.com>
2020-04-28 07:02:57 +0000

    Rework sellingo importer to use sellasist API

--- sellingo_pkg/sellingo/importer.py
+++ sellingo_pkg/sellingo/importer.py
@@ -1,153 +1,112 @@
-import numpy as np
-import olefile
-import pandas as pd
+import logging
+from decimal import Decimal
+from typing import List
+
+import requests
 from markdownify import markdownify as md
 
 from gateway.models import Product
 
+LOGGER = logging.getLogger(__name__)
+
 
 # pylint: disable=too-few-public-methods
 class Sellingo:
-    def __init__(self, filepath, shop_url):
-        self.shop_url = shop_url
-        self.filepath = filepath
-        self.data_frame = None
-        self.products_count = 0
-        self.current_product_index = 0
-        self.products = dict()
+    def __init__(self, api_url, api_key):
+        self.api_url = api_url
+        self.session = requests.Session()
+        self.session.headers.update({"apiKey": api_key})
+
+    def _get(self, endpoint: str) -> dict:
+        response = self.session.get(f"{self.api_url}/{endpoint}")
+        return response.json()
+
+    def _get_products_data(self) -> dict:
+        return self._get("products/")
+
+    def _get_product_details_data(self, product_id) -> dict:
+        return self._get(f"products/{product_id}/")
 
     @staticmethod
-    def _read_file(filepath) -> pd.DataFrame:
-        with open(filepath, "rb") as file:
-            ole = olefile.OleFileIO(file)
-            if ole.exists("Workbook"):
-                stream = ole.openstream("Workbook")
-                return pd.read_excel(stream, engine="xlrd")
-            raise IOError
+    def _clean_group_name(group_name: str) -> str:
+        group_name = group_name.lower()
+        group_name = group_name.replace("kolor", "color")
+        group_name = group_name.replace("rozmiar", "size")
+        return group_name
+
+    def _get_variant_data(self, product_details: dict, variant_details: dict) -> dict:
+        group = self._get(f"variants_groups/{product_details['variants_group_id']}/")
+        group_name = self._clean_group_name(group["name"])
+        return {group_name: variant_details["properties"][0]["name"]}
 
     @staticmethod
-    def _fill_empty_ids(data_frame):
-        """
-        File exported by Sellingo looks like this:
+    def _get_variant_sku(product_details: dict, variant: dict) -> str:
+        variant_id = f"{variant['id']};" if variant else ""
+        sku = variant["catalog"] if variant else product_details["catalog_number"]
+        product_id = f"{product_details['id']};"
+        return f"{product_id}{variant_id}{sku}"
 
-        1;Base product;...
-         ;Variant of base product;...
-
-         ;Another variant of base product;...
-        2;Another base product;...
-         ;Variant of another base product;...
-
-        So in order to easier group product variants with product this function creates new_id,
-        which is assigned to both base product and its variants.
-        :param data_frame:
-        :return:
-        """
-        data_frame["new_id"] = data_frame["ID produktu"]
-        data_frame["new_id"] = data_frame["new_id"].dropna().diff()
-        data_frame.loc[0, "new_id"] = data_frame.loc[0, "ID produktu"]
-        data_frame["new_id"] = data_frame["new_id"].fillna(0).cumsum()
-        data_frame["new_id"] = data_frame["new_id"].astype({"new_id": "int32"})
-        return data_frame
-
-    @staticmethod
-    def _is_value_nan(value):
-        try:
-            return np.isnan(value)
-        except TypeError:
-            return False
-
-    def __get_images_from_images_string(self, images_string):
-        """
-        Example value of images string from Sellingo file looks like this:
-
-        'public/upload/catalog/product/13/minigallery/original_13973135681.jpg;
-        public/upload/catalog/product/13/minigallery/original_13973135722.jpg'
-
-        So this function splits it and appends shop url at the beginning
-
-        :param images_string:
-        :return:
-        """
-        return [f"{self.shop_url}{url}" for url in images_string.split(";")]
-
-    def __get_initial_product(self, initial_product: pd.Series) -> Product:
-        images = (
-            self.__get_images_from_images_string(initial_product.images)
-            if not self._is_value_nan(initial_product.images)
-            else []
-        )
-        name = initial_product.title
-        price = initial_product.price_brutto
-        vat = initial_product.vat
-        description = md(initial_product.text) if not self._is_value_nan(initial_product.text) else ""
-        description_short = md(initial_product.textshort) if not self._is_value_nan(initial_product.textshort) else ""
-        return Product(
-            name=name,
-            price=price,
-            vat_percent=vat,
-            description_short=description_short,
+    def _get_product(
+        self, product_details: dict, description: str, description_short: str, variant: dict = None
+    ) -> Product:
+        images = [x["original"] for x in product_details["images"]]
+        product_price = Decimal(str(product_details["price"]))
+        sku = self._get_variant_sku(product_details, variant)
+        product = Product(
+            name=product_details["title"],
+            price=product_price,
+            vat_percent=int(product_details["vat"]),
+            stock=Decimal(str(product_details["quantity"])),
             description=description,
+            description_short=description_short,
             images_urls=images,
+            sku=sku,
         )
+        if variant:
+            product.variant_data = self._get_variant_data(product_details, variant)
+            variant_price = Decimal(str(variant["price"]))
+            product.price = variant_price if variant_price != Decimal(0) else product_price
+            product.stock = Decimal(str(variant["quantity"]))
+        return product
 
-    @staticmethod
-    def __get_variant_data(variants):
-        variant_data_list = variants.split("#")[-2:]
-        variant_data = dict()
-        for variant_data_element in variant_data_list:
-            variant_data_value = variant_data_element.split(":")
-            variant_data_value[0] = variant_data_value[0].replace("Rozmiar", "size")
-            variant_data_value[0] = variant_data_value[0].replace("Kolor", "color")
-            variant_data_value[1] = variant_data_value[1].replace("Rozmiar ", "")
-            variant_data[variant_data_value[0]] = variant_data_value[1]
-        return variant_data
-
-    def __get_variant(self, variant: pd.Series, initial_product: Product) -> Product:
-        variant_images = (
-            self.__get_images_from_images_string(variant.images)
-            if not self._is_value_nan(variant.images)
-            else initial_product.images_urls
-        )
-        variant_name = variant.title if not self._is_value_nan(variant.title) else initial_product.name
-        variant_price = variant.price_brutto if not np.isnan(variant.price_brutto) else initial_product.price
-        variant_description = md(variant.text) if not self._is_value_nan(variant.text) else initial_product.description
-        variant_description_short = (
-            md(variant.textshort) if not self._is_value_nan(variant.textshort) else initial_product.description_short
-        )
-        variant_vat = variant.vat if not self._is_value_nan(variant.vat) else initial_product.vat_percent
-        variant_stock = variant.quantity
-        variant_data = self.__get_variant_data(variant.variants)
-        return Product(
-            name=variant_name,
-            price=variant_price,
-            vat_percent=variant_vat,
-            stock=variant_stock,
-            description=variant_description,
-            description_short=variant_description_short,
-            images_urls=variant_images,
-            variant_data=variant_data,
-        )
-
-    def _set_variant(self, variant_data: pd.Series, initial_product: Product, product_id: int):
-        variant = self.__get_variant(variant_data, initial_product)
-        self.products[product_id].append(variant)
-
-    def _parse_to_product(self, element: pd.DataFrame):
-        self.current_product_index += 1
-        print(f"Importing: {self.current_product_index} / {self.products_count}")
-        initial_product = self.__get_initial_product(element.iloc[0])
-        product_id = element.iloc[0].new_id
-        initial_variants = element.iloc[1:]
-        self.products[product_id] = []
-        initial_variants.apply(self._set_variant, args=(initial_product, product_id), axis=1)
-
-    def _parse_df(self):
-        grouped_df = self.data_frame.groupby("new_id")
-        self.products_count = grouped_df.ngroups
-        grouped_df.apply(self._parse_to_product)
+    def _get_single_product_variants(self, input_product: dict) -> List[Product]:
+        product_details = self._get_product_details_data(input_product["id"])
+        description = ""
+        description_short = ""
+        datacell_name_for_long_description = "Długi opis"
+        datacell_name_for_short_description = "Krótki opis"
+        for description_data in product_details["description"]:
+            if description_data["datacell_name"] == datacell_name_for_long_description:
+                description = md(description_data["content"])
+            if description_data["datacell_name"] == datacell_name_for_short_description:
+                description_short = md(description_data["content"])
+        if "variants" not in product_details:
+            return [self._get_product(product_details, description, description_short)]
+        return [
+            self._get_product(product_details, description, description_short, variant)
+            for variant in product_details["variants"]
+        ]
 
     def build_products(self):
-        input_data_frame = self._read_file(self.filepath)
-        self.data_frame = self._fill_empty_ids(input_data_frame)
-        self._parse_df()
-        return list(self.products.values())
+        """
+        First yields number of products then List[Product]
+        :return:
+        """
+        products = self._get_products_data()
+        yield len(products)
+        for product in products:
+            yield self._get_single_product_variants(product)
+
+    @staticmethod
+    def _get_product_id_and_variant_id_from_sku(sku: str) -> List[str]:
+        if sku.count(";") > 2:
+            LOGGER.warning("SKU contains more then 2 `;`: [%s]", sku)
+        return sku.split(";")[:2]
+
+    def get_product_stock_level(self, sku: str) -> Decimal:
+        product_id, variant_id = self._get_product_id_and_variant_id_from_sku(sku)
+        product = self._get_product_details_data(product_id)
+        if variant_id and "variants" in product:
+            variant = next(x for x in product["variants"] if x["id"] == variant_id)
+            return Decimal(str(variant["quantity"]))
+        return Decimal(str(product["quantity"]))
--- sellingo_pkg/setup.py
+++ sellingo_pkg/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="sellingo",
     py_modules=["importer", "runner"],
-    install_requires=["olefile", "pandas", "markdownify", "xlrd", "gateway"],
+    install_requires=["requests", "markdownify", "gateway"],
     python_requires=">=3.7",
     packages=["sellingo"],
 )
