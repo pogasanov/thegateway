9d1854812d3144896aaca8dc1cc765214d72f476
Marko Hannila <marko.hannila@thegateway.io>
2020-05-05 15:59:15 +0300

    Implement tool for generating base for category mapping file

---
+++ idosell_pkg/idosell/category_mappings_eprosport.pl.py
@@ -0,0 +1,47 @@
+category_mappings = {
+    "0": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "*Kategoria tymczasowa",
+    },
+    "1214553801": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Kategoria testowa 2",
+    },
+    "1214553839": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Kategoria testowa 1/Podkategoria 1",
+    },
+    "1214553855": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Kategoria testowa 1/Podkategoria 2",
+    },
+    "1214553885": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Odzież",
+    },
+}
---
+++ idosell_pkg/idosell/category_mappings_telimena.pl.py
@@ -0,0 +1,335 @@
+category_mappings = {
+    "0": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "*Kategoria tymczasowa",
+    },
+    "1214553883": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Miękkie",
+    },
+    "1214553884": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Miękkie na fiszbinie",
+    },
+    "1214553885": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Usztywniane",
+    },
+    "1214553886": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Ciąża, karmienie",
+    },
+    "1214553888": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Odpinane ramiączka",
+    },
+    "1214553889": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Sportowe",
+    },
+    "1214553891": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Stringi",
+    },
+    "1214553892": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Tanga",
+    },
+    "1214553893": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Hipster",
+    },
+    "1214553894": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Tai",
+    },
+    "1214553895": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Midi",
+    },
+    "1214553896": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Maxi",
+    },
+    "1214553898": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Koszulki",
+    },
+    "1214553899": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna modelująca",
+    },
+    "1214553907": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Stroje kąpielowe/Jednoczęściowe",
+    },
+    "1214553908": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Stroje kąpielowe/Dwuczęściowe",
+    },
+    "1214553910": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna nocna/Piżamy",
+    },
+    "1214553911": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna nocna/Koszule nocne",
+    },
+    "1214553912": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna nocna/Szlafroki",
+    },
+    "1214553913": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna nocna/Dresy",
+    },
+    "1214553916": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Akcesoria/Ramiączka",
+    },
+    "1214553918": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Akcesoria/Pasy do pończoch",
+    },
+    "1214553920": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Biustonosze/Push up",
+    },
+    "1214553923": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna męska/Piżamy",
+    },
+    "1214553924": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna męska/Slipy",
+    },
+    "1214553925": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna męska/Bokserki",
+    },
+    "1214553926": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna męska/Kąpielówki",
+    },
+    "1214553927": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Mini",
+    },
+    "1214553928": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Majtki damskie/Short",
+    },
+    "1214553929": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Stroje kąpielowe/Sloggi Swim",
+    },
+    "1214554083": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Stroje kąpielowe/Mix To Match",
+    },
+    "1214554085": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna męska/Koszulki",
+    },
+    "1214554088": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Body",
+    },
+    "1214554091": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Akcesoria/Akcesoria do biustonoszy",
+    },
+    "1214554094": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Bielizna nocna/Legginsy",
+    },
+    "1214554107": {
+        "categories": [
+            {
+                "EN": "",
+                "PL": "",
+            },
+        ],
+        "src_name": "Maseczki  ochronne",
+    },
+}
