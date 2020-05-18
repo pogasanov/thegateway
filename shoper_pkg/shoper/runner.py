b544197ee7a4e79d4cb21847ca5e68b5ca37b3f9
Marko Hannila <marko.hannila@thegateway.io>
2020-05-18 16:03:14 +0300

    Remove the unnecessary `IMAGEBUCKET_URL` env variable from shoper importer

--- shoper_pkg/shoper/runner.py
+++ shoper_pkg/shoper/runner.py
@@ -12,11 +12,10 @@ load_dotenv(env_path)
 GATEWAY_BASE_URL = os.environ.get("GATEWAY_BASE_URL", "https://sma.dev.gwapi.eu")
 GATEWAY_SHOP_ID = os.environ.get("GATEWAY_SHOP_ID", "f99424a4-d79b-4e37-aa86-f2ed84c27d7b")
 GATEWAY_SECRET = os.environ.get("GATEWAY_SECRET", "/ew7hc6im+sQOHrQK8S0cZ9UXn35lka1SqCrLVRpJKM=")
-IMAGE_URL_PREFIX = f"{os.environ.get('IMAGEBUCKET_URL')}{GATEWAY_SHOP_ID}/"
 SHOPER_BASE_URL = os.environ.get("SHOPER_BASE_URL", "http://sklep266192.shoparena.pl")
 SHOPER_USERNAME = os.environ.get("SHOPER_USERNAME", "karol.ziolkowski@profil-software.com")
 SHOPER_PASSWORD = os.environ.get("SHOPER_PASSWORD", "Magik123")
-gateway = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET, IMAGE_URL_PREFIX)
+gateway = Gateway(GATEWAY_BASE_URL, GATEWAY_SHOP_ID, GATEWAY_SECRET)
 importer = Shoper(
     SHOPER_BASE_URL,
     SHOPER_USERNAME,
